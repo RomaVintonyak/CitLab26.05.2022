c1ad16395e9e32f852db230810eb7c5ee503814f
RomaVintonyak <19roman23@gmail.com>
2022-05-26 16:03:29 +0300

    create intro & posts block

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 jQuery(document).ready(function () {
   "use script";
- 
+  //burger btn 
+  var burgerBtn = $("#burgerBtn");
+  burgerBtn.on("click", function (event) {
+    event.preventDefault();
+    $(this).toggleClass("burgerBtn--active");
+  });
+  /*post block*/
+  var dropDownBtn = $(".drop__down--header");
+  dropDownBtn.on("click", function () {
+    $(this).toggleClass("header--active");
+    $(this).parent(".drop__down").find(".drop__down--list").toggleClass("show__list");
+  });
 });
